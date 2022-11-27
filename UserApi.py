04b2e2f3393110d9e7ce27f93dba043e0e499fda
sty0507 <styoung0507@gmail.com>
2022-11-27 21:24:02 +0900

    데이터를 xml 형태로 가져옴

--- UserApi.py
+++ UserApi.py
@@ -1,4 +1,5 @@
 import urllib.request
+import xml.etree.ElementTree as ET
 
 # https://developers.naver.com/apps/#/wizard/register
 # 여기서 인증하고 발급받기
@@ -6,9 +7,13 @@ import urllib.request
 client_id = '53Zlk_08EPnrNtdMMjqB'
 client_secret = 'uEYc1RIM1z'
 
-text = input("검색할 단어를 입력하세요 : ")
+data_list = []
+
+# text = input("검색할 단어를 입력하세요 : ")
+text = "월드컵"
 encText = urllib.parse.quote(text)
-url = "https://openapi.naver.com/v1/search/blog?query=" + encText
+# url = "https://openapi.naver.com/v1/search/blog?query=" + encText
+url = "https://openapi.naver.com/v1/search/blog.xml?query=" + encText
 
 request = urllib.request.Request(url)
 request.add_header('X-Naver-Client-Id', client_id)
@@ -17,6 +22,12 @@ response = urllib.request.urlopen(request)
 rescode = response.getcode()
 if(rescode==200):
     response_body = response.read()
-    print(response_body.decode('utf-8'))
+    data = response_body.decode('utf-8')
 else:
     print("Error Code:" + rescode)
+# data_list = data.split()
+# print(data_list)
+root = ET.fromstring(data)
+for d in root.findall("title"):
+    print(d.attrib)
+    print(d.attrib.get("title"))
